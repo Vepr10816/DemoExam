955c68c7888c4597e479f6b3dab160a5e6672248
Vepr10816 <91605518+Vepr10816@users.noreply.github.com>
2023-05-22 00:28:45 +0300

    Задание

---
+++ Session/Сессия 1/mssql_script_trade.sql
@@ -0,0 +1,49 @@
+create database [Trade]
+go
+use [Trade]
+go
+create table [Role]
+(
+	RoleID int primary key identity,
+	RoleName nvarchar(100) not null
+)
+go
+create table [User]
+(
+	UserID int primary key identity,
+	UserSurname nvarchar(100) not null,
+	UserName nvarchar(100) not null,
+	UserPatronymic nvarchar(100) not null,
+	UserLogin nvarchar(max) not null,
+	UserPassword nvarchar(max) not null,
+	UserRole int foreign key references [Role](RoleID) not null
+)
+go
+create table [Order]
+(
+	OrderID int primary key identity,
+	OrderStatus nvarchar(max) not null,
+	OrderDeliveryDate datetime not null,
+	OrderPickupPoint nvarchar(max) not null
+)
+go
+create table Product
+(
+	ProductArticleNumber nvarchar(100) primary key,
+	ProductName nvarchar(max) not null,
+	ProductDescription nvarchar(max) not null,
+	ProductCategory nvarchar(max) not null,
+	ProductPhoto image not null,
+	ProductManufacturer nvarchar(max) not null,
+	ProductCost decimal(19,4) not null,
+	ProductDiscountAmount tinyint null,
+	ProductQuantityInStock int not null,
+	ProductStatus nvarchar(max) not null,
+)
+go
+create table OrderProduct
+(
+	OrderID int foreign key references [Order](OrderID) not null,
+	ProductArticleNumber nvarchar(100) foreign key references Product(ProductArticleNumber) not null,
+	Primary key (OrderID,ProductArticleNumber)
+)
